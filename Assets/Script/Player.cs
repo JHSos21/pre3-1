8f9558ff2bf8615cbbdd84b0ec1ff9953cdd2a2e
JHSos21 <hyeonseong2190@gmail.com>
2025-03-20 23:45:52 +0900

    playermove
    코드 정리

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -13,8 +13,18 @@ public class Player : MonoBehaviour
     }
     void Update()
     {
+        PlayerMove();
+        SpeedLimit();
+        StopSliding();
+        ChangeGravity();
+    }
+    void PlayerMove()
+    {
         float move = Input.GetAxisRaw("Horizontal");
         rigid.AddForce(Vector2.right * move, ForceMode2D.Impulse);
+    }
+    void SpeedLimit()
+    {
         if (rigid.linearVelocity.x > maxSpeed)
         {
             rigid.linearVelocity = new Vector2(maxSpeed, rigid.linearVelocity.y);
@@ -23,17 +33,24 @@ public class Player : MonoBehaviour
         {
             rigid.linearVelocity = new Vector2(-1 * maxSpeed, rigid.linearVelocity.y);
         }
+    }
+    void StopSliding()
+    {
         if (Input.GetButtonUp("Horizontal"))
         {
             rigid.linearVelocity = new Vector2(0, rigid.linearVelocity.y);
         }
+    }
+    void ChangeGravity()
+    {
         if (Input.GetButtonDown("Jump"))
         {
             if (rigid.gravityScale == 1.5F)
             {
                 rigid.gravityScale = -1.5F;
             }
-            else{
+            else
+            {
                 rigid.gravityScale = 1.5F;
             }
         }
